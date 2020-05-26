57b0879958feb57538999542e29adf2ef83abb15
Jeff Arce <jeff.arce@upraxis.com>
2020-05-27 00:47:02 +0800

    Add GlobalCases Model.

---
+++ app/src/main/java/com/jeff/covidtracker/database/local/GlobalCases.kt
@@ -0,0 +1,26 @@
+package com.jeff.covidtracker.database.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = GlobalCases.TABLE_NAME)
+data class GlobalCases (
+
+    @PrimaryKey
+    @ColumnInfo(name = "date")
+    var date: String,
+    @ColumnInfo(name = "new_cases")
+    var newCases: NewCases? = null,
+    @ColumnInfo(name = "total_cases")
+    var totalCases: TotalCases? = null
+) {
+
+    companion object {
+
+        const val COLUMN_DEAL_ID = "global_cases_id"
+        const val COLUMN_ID = "id"
+        const val TABLE_NAME = "global_cases"
+    }
+
+}
