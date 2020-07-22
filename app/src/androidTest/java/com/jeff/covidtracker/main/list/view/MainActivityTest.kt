653d89eabb108c8d0375ad9de88d7dfddd46cd02
jeff <jeffandrew.s.arce@gmail.com>
2020-07-22 18:16:35 +0800

    Add MainActivityTest test cases.

--- app/src/androidTest/java/com/jeff/covidtracker/main/list/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/jeff/covidtracker/main/list/view/MainActivityTest.kt
@@ -1,5 +1,55 @@
 package com.jeff.covidtracker.main.list.view
 
-import org.junit.Assert.*
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.jeff.covidtracker.R
+import com.jeff.covidtracker.main.RecyclerViewItemCountAssertion
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.greaterThan
+import org.junit.Assert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class MainActivityTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule @JvmField
+    var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun brazil() {
+        onView(withId(R.id.customRecyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                1, click()));
+
+
+        onView(
+            CoreMatchers.allOf(
+                CoreMatchers.instanceOf(TextView::class.java),
+                ViewMatchers.withParent(withId(R.id.country_detail_toolbar))
+            )
+        )
+            .check(ViewAssertions.matches(ViewMatchers.withText("Brazil")))
+    }
+
+    @Test
+    fun countryListIsNotEmpty() {
+        onView(withId(R.id.customRecyclerView)).check(
+            RecyclerViewItemCountAssertion.withItemCount(greaterThan(0) as Matcher<Int?>?)
+        )
+    }
+
+}
