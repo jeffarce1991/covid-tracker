2a001a17bcc37c1243c5d1706341be8213025e8b
jeff <jeffandrew.s.arce@gmail.com>
2020-08-28 16:05:21 +0800

    Add ShowError() SnackBar in SplashActivity.

--- app/src/main/java/com/jeff/covidtracker/main/splash/view/SplashActivity.kt
+++ app/src/main/java/com/jeff/covidtracker/main/splash/view/SplashActivity.kt
@@ -3,14 +3,19 @@ package com.jeff.covidtracker.main.splash.view
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
 import com.hannesdorfmann.mosby.mvp.MvpActivity
 import com.jeff.covidtracker.R
+import com.jeff.covidtracker.databinding.ActivitySplashBinding
 import com.jeff.covidtracker.main.dashboard.view.DashboardActivity
 import com.jeff.covidtracker.main.splash.presenter.SplashPresenter
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
 class SplashActivity : MvpActivity<SplashView, SplashPresenter>(), SplashView {
+
+    private lateinit var binding: ActivitySplashBinding
 
     @Inject
     lateinit var splashPresenter: SplashPresenter
@@ -19,6 +24,7 @@ class SplashActivity : MvpActivity<SplashView, SplashPresenter>(), SplashView {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         splashPresenter.decideWhichScreenToRedirectTo()
     }
 
@@ -32,5 +38,13 @@ class SplashActivity : MvpActivity<SplashView, SplashPresenter>(), SplashView {
             Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
                     or FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
+    }
+
+    override fun showError(message: String) {
+        Snackbar.make(binding.coordLayout,
+            message,
+            Snackbar.LENGTH_INDEFINITE)
+            .setAction("Retry") {splashPresenter.decideWhichScreenToRedirectTo()}
+            .show()
     }
 }
